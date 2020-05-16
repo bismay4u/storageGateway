57236c366346f3036dc69151c8ed83254a608d71
Bismay Kumar Mohapatra <bismay4u@gmail.com>
2020-05-16 17:16:35 +0530

    Latest updates and fixes

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# storageGateway
+# Storage Gateway
+
+A configurable Cloud Storage Gateway that can also forward requests to targeted servers.
+
+Mostly while handling Base64 photos or binaries, we can upload the same directly to the cloud media servers, this project takes care of the interim parameters, converts the base64 data into binary files, stores them with cloud provider, returns the URI or forwards the URI to respective forward servers.
+
+
+Thank you
+Bismay
---
+++ api.php
@@ -0,0 +1,97 @@
+<?php
+if(!defined("ROOT")) die("This file can't be accessed directly");
+
+if(!function_exists("save_base64_image")) {
+  
+  function forwardPhoto($forwardURI, $photoURI, $token = false) {
+    $POSTDATAPARAM = $_ENV['POSTDATAPARAM'];
+
+    $_POST[$POSTDATAPARAM] = $photoURI;
+
+    $curl = curl_init();
+
+    $header = [
+      "Cache-Control: no-cache",
+      "Content-Type: application/x-www-form-urlencoded",
+    ];
+    if($token) {
+        $header[] = "token: {$token}";
+    }
+
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => $forwardURI,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 30,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "POST",
+      CURLOPT_POSTFIELDS => http_build_query($_POST),
+      CURLOPT_HTTPHEADER => $header
+    ));
+
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+    curl_close($curl);
+    //var_dump([$response,$err]);
+
+    return $response;
+  }
+
+  function uploadPhotoToS3($S3Config) {
+    $tempDir = $_ENV['TEMPDIR'];
+    $POSTDATAPARAM = $_ENV['POSTDATAPARAM'];
+
+    if(isset($_POST[$POSTDATAPARAM])) {
+      $S3Params=$S3Config[$_GET['s3key']];
+
+      $fname=md5(time().rand());
+      $finalFile = save_base64_image($_POST[$POSTDATAPARAM],$fname,$tempDir);
+
+      S3::$useSSL = false;
+      $s3 = new S3($S3Params['accessKeyId'], $S3Params['secretAccessKey']);
+
+      if(!$s3->putBucket($S3Params['bucket'], S3::ACL_PUBLIC_READ)) {
+          exit("Sorry, Storage Initiation failed");
+      }
+
+      $uploadPath="{$S3Params['folder']}/".time()."_".$finalFile;
+      $b=$s3->putObjectFile($tempDir.$finalFile, $S3Params['bucket'], $uploadPath, $S3Params['security_policy']);
+
+      $photoURI='https://s3.amazonaws.com/'.$S3Params['bucket'].'/'.$uploadPath;
+      
+      unlink($tempDir.$finalFile);
+
+      if($b) {
+        return $photoURI;
+      } else {
+        die("Error uploading image to cloud target");
+      }
+    } else {
+      die("Image Data Missing or Error Configuring Post Paramters");
+    }
+  }
+  
+  function save_base64_image($base64_image_string, $output_file_without_extension, $path_with_end_slash="" ) {
+      //usage:  if( substr( $img_src, 0, 5 ) === "data:" ) {  $filename=save_base64_image($base64_image_string, $output_file_without_extentnion, getcwd() .     "/application/assets/pins/$user_id/"); }      
+      //
+      //data is like:    data:image/png;base64,asdfasdfasdf
+      $splited = explode(',', substr( $base64_image_string , 5 ) , 2);
+      $mime=$splited[0];
+      $data=$splited[1];
+
+      $mime_split_without_base64=explode(';', $mime,2);
+      $mime_split=explode('/', $mime_split_without_base64[0],2);
+      if(count($mime_split)==2)
+      {
+          $extension=$mime_split[1];
+          if($extension=='jpeg') $extension='jpg';
+          //if($extension=='javascript')$extension='js';
+          //if($extension=='text')$extension='txt';
+          $output_file_with_extension=$output_file_without_extension.'.'.$extension;
+      }
+      file_put_contents( $path_with_end_slash . $output_file_with_extension, base64_decode($data) );
+      return $output_file_with_extension;
+  }
+}
+?>
